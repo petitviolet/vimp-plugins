37b828af18cbc2a2fd1fe248753f2cceb51b51be
petitviolet <violethero0820@gmail.com>
2014-07-01 20:08:28 +0900

    use prototype

--- speakerdeck.js
+++ speakerdeck.js
@@ -1,24 +1,28 @@
 (function() {
+  var IframePlayer = function(iframe) {
+    this.iframe = iframe;
+    this.player = iframe.contentWindow.player;
+  };
+  IframePlayer.prototype = {
+    next: function() {
+      this.player.nextSlide();
+    },
+    prev: function() {
+      this.player.previousSlide();
+    },
+    fullscreen: function() {
+      this.iframe.mozRequestFullScreen();
+    }
+  };
+
   var HandleSpeakerdeck = function() {
     var doc = content.document;
     if (doc.location.host !== 'speakerdeck.com') {
       return liberator.echoerr('This is not speakerdeck...');
     }
-
     var iframe = doc.wrappedJSObject.querySelector('iframe.speakerdeck-iframe');
-    var player = iframe.contentWindow.player;
-
-    return {
-      next: function() {
-        player.nextSlide();
-      },
-      prev: function() {
-        player.previousSlide();
-      },
-      fullscreen: function() {
-        iframe.mozRequestFullScreen();
-      }
-    }
+    var iframePlayer = new IframePlayer(iframe);
+    return iframePlayer;
   };
 
   liberator.modules.commands.addUserCommand(
@@ -27,9 +31,12 @@
     function() {},
     {
       subCommands: [
-        new Command(['n[ext]'], 'Go next page', function() {HandleSpeakerdeck().next()}),
-        new Command(['p[rev]'], 'Go prev page', function() {HandleSpeakerdeck().prev()}),
-        new Command(['f[ullscreen]'], 'Toggle fullscreen', function() {HandleSpeakerdeck().fullscreen()})
+        new Command(['n[ext]'], 'Go next page',
+                    function() {HandleSpeakerdeck().next()}),
+        new Command(['p[rev]'], 'Go prev page',
+                    function() {HandleSpeakerdeck().prev()}),
+        new Command(['f[ullscreen]'], 'Toggle fullscreen',
+                    function() {HandleSpeakerdeck().fullscreen()})
       ]
     },
     true
