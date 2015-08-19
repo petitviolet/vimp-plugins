23c21550e30578a33c18a5b6b10a0deea28ab419
petitviolet <violethero0820@gmail.com>
2015-08-19 13:51:59 +0900

    implement qiita search with "qiita <query>"

---
+++ qiitaSearch.js
@@ -0,0 +1,22 @@
+(function() {
+  liberator.modules.commands.addUserCommand(
+    ["qiita"],
+    "search from qiita",
+    function(args){
+      if (args.length === 0) {
+        liberator.echo('input a query!');
+        return false;
+      }
+
+      // make "and" query
+      var query = args.join("+");
+      var youtubeUrl = 'http://qiita.com/search?utf8=✓&sort=rel&q='
+      // open in a new tab
+      liberator.open(youtubeUrl + query, liberator.NEW_TAB);
+      return false;
+    },
+    {},
+    true
+  );
+})();
+
