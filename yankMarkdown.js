f0b1f00adda12ec763d55ec4681992b3f1b382b3
petitviolet <violethero0820@gmail.com>
2017-01-21 21:28:50 +0900

    refactor and fix bug

--- yankMarkdown.js
+++ yankMarkdown.js
@@ -1,4 +1,62 @@
 (function() {
+
+  function makeMarkdownString(window, url) {
+    var selection = getUserSelection(window);
+    var markdown = "";
+    if (typeof selection === "undefined" || selection.length === 0) {
+      var title = buffer.title;
+      if (isGithub(url)) {
+        // [petitviolet/vimp-plugins: My Vimperator plugins]
+        //   -> [petitviolet/vimp-plugins]
+        // [vimp-plugins/yankMarkdown.js at master · petitviolet/vimp-plugins](https://github.com/petitviolet/vimp-plugins/blob/master/yankMarkdown.js)
+        //   -> [petitviolet/vimp-plugins/yankmarkdown.js]
+        title = extractGithubTitle(url);
+      }
+      markdown = `[${title}](${url})`;
+    } else {
+      markdown = `[${selection}](${url})`;
+    }
+    return markdown.trim();
+  }
+
+  function getUserSelection(_window) {
+    var selection = new XPCNativeWrapper(_window.content.window).getSelection().toString().trim();
+    return selection;
+  }
+
+  function isGithub(url) {
+    return url.includes("github.com");
+  }
+
+  function extractGithubTitle(url) {
+    var segments = url.split('/');
+    var title = "";
+    if (segments.length === 5) {
+      // repository top
+      // https://github.com/petitviolet/vimp-plugins
+      // -> petitviolet/vimp-plugins
+      title = githubRepository(segments);
+    } else if (segments.length > 5) {
+      // a file in a repository
+      // https://github.com/petitviolet/vimp-plugins/blob/master/yankMarkdown.js
+      // -> petitviolet/vimp-plugins/yankmarkdown.js
+      var repo = githubRepository(segments);
+      var file = segments[segments.length - 1];
+      title = `${repo}/${file}`;
+    }
+
+    if (title[title.length - 1] === "/") {
+      // trim trailing `/`
+      title = title.slice(0, title.length - 1);
+    }
+    return title;
+  }
+
+  function githubRepository(segments) {
+    return segments.slice(3, 5).join('/');
+  }
+
+  // install command
   liberator.modules.commands.addUserCommand(
     ["yankmarkdown", 'ymd'],
     "yank webpage title and url as markdown",
@@ -17,58 +75,3 @@
   );
 })();
 
-function makeMarkdownString(window, url) {
-  var selection = getSelection(window);
-  var markdown = "";
-  if (typeof selection === "undefined" || selection.length === 0) {
-    var title = buffer.title;
-    if (isGithub(url)) {
-      // [petitviolet/vimp-plugins: My Vimperator plugins]
-      //   -> [petitviolet/vimp-plugins]
-      // [vimp-plugins/yankMarkdown.js at master · petitviolet/vimp-plugins](https://github.com/petitviolet/vimp-plugins/blob/master/yankMarkdown.js)
-      //   -> [petitviolet/vimp-plugins/yankmarkdown.js]
-      title = extractGithubTitle(url);
-    }
-    markdown = `[${title}](${url})`;
-  } else {
-    markdown = `[${selection}](${url})`;
-  }
-  return markdown;
-}
-
-function getSelection(window) {
-  var selection = new XPCNativeWrapper(window.content.window).getSelection().toString();
-  return selection;
-}
-
-function isGithub(url) {
-  return url.includes("github.com");
-}
-
-function extractGithubTitle(url) {
-  var segments = url.split('/');
-  var title = "";
-  if (segments.length === 5) {
-    // repository top
-    // https://github.com/petitviolet/vimp-plugins
-    // -> petitviolet/vimp-plugins
-    title = githubRepository(segments);
-  } else if (segments.length > 5) {
-    // a file in a repository
-    // https://github.com/petitviolet/vimp-plugins/blob/master/yankMarkdown.js
-    // -> petitviolet/vimp-plugins/yankmarkdown.js
-    var repo = githubRepository(segments);
-    var file = segments[segments.length - 1];
-    title = `${repo}/${file}`;
-  }
-
-  if (title[title.length - 1] === "/") {
-    // trim trailing `/`
-    title = title.slice(0, title.length - 1);
-  }
-  return title;
-}
-
-function githubRepository(segments) {
-    return segments.slice(3, 5).join('/');
-}
