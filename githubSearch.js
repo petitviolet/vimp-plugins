a77212637d6082a53f41fceefe2262a0916cbc83
komurasaki <komurasaki@fringe81.com>
2017-01-23 16:26:34 +0900

    enable empty repository

--- githubSearch.js
+++ githubSearch.js
@@ -3,13 +3,13 @@
     ["git[hubSearch]"],
     "search from github repository, usage like 'git[hubSearch] petitviolet/vimp-plugings'",
     function(args){
-      if (args.length === 0) {
-        liberator.echo('input a query like [username]/[repository]!');
-        return false;
-      }
-
+      // if (args.length === 0) {
+      //   liberator.echo('input a query like [username]/[repository]!');
+      //   return false;
+      // }
+      //
       // var query = args[0].split('/');
-      var query = args[0];
+      var query = args[0] || '';
       var githubUrl = 'https://github.com/' + query;
       // open in a new tab
       liberator.open(githubUrl, liberator.NEW_TAB);
